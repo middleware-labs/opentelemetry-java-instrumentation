edb6c5c38d9390e4386423fa5ee23c289b0df454
Keval Bhogayata <90185475+bhogayatakb@users.noreply.github.com>
2023-06-22 14:27:31 +0530

    added custom log attribute for filtering + removed extra class from extension (#2)

---
+++ examples/extension/src/main/java/com/example/javaagent/DemoLogRecordProcessor.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.example.javaagent;
+
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.sdk.logs.LogRecordProcessor;
+import io.opentelemetry.sdk.logs.ReadWriteLogRecord;
+
+/**
+ * See <a
+ * href="https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/logs/sdk.md#logrecordprocessor">
+ * OpenTelemetry Specification</a> for more information about {@link LogRecordProcessor}.
+ *
+ * @see DemoAutoConfigurationCustomizerProvider
+ */
+public class DemoLogRecordProcessor implements LogRecordProcessor {
+  private static final DemoLogRecordProcessor INSTANCE = new DemoLogRecordProcessor();
+
+  static DemoLogRecordProcessor getInstance() {
+    return INSTANCE;
+  }
+
+  public DemoLogRecordProcessor() {}
+
+  @Override
+  public void onEmit(Context context, ReadWriteLogRecord logRecord) {
+    logRecord.setAttribute(AttributeKey.stringKey("mw.app.lang"), "java");
+  }
+
+  @Override
+  public String toString() {
+    return "DemoLogRecordProcessor";
+  }
+}
