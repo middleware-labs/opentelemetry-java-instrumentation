8368ff6a8e78f88c7d85e94b7d8fdea377207f56
parth-dadhaniya <parth@middleware.io>
2023-07-18 18:01:42 +0530

    [pyro-auth]: Added authentication in Pyroscope Profiling.

--- examples/extension/src/main/java/com/example/javaagent/config/EnvironmentConfig.java
+++ examples/extension/src/main/java/com/example/javaagent/config/EnvironmentConfig.java
@@ -11,8 +11,11 @@ public class EnvironmentConfig {
   public static final String MW_PROFILING_SERVER_URL =
       System.getenv().getOrDefault("MW_PROFILING_SERVER_URL", "https://profiling.middleware.io");
 
-  public static final String TARGET = System.getenv().getOrDefault("TARGET", null);
-
   public static final String MW_AGENT_SERVICE =
       System.getenv().getOrDefault("MW_AGENT_SERVICE", "localhost");
+
+  public static final String MW_AUTH_URL =
+      System.getenv().getOrDefault("MW_AUTH_URL", "https://app.middleware.io/api/v1/auth");
+
+  public static final String MW_API_KEY = System.getenv().getOrDefault("MW_API_KEY", null);
 }
