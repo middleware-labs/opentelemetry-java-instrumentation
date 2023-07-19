e8243cdbd8a618ee4d10675f194c84ce8ab522c7
parth-dadhaniya <parth@middleware.io>
2023-07-19 15:05:11 +0530

    [update-auth]: Updated error handling in Pyroscope Profiling.

--- examples/extension/src/main/java/com/example/profile/PyroscopeProfile.java
+++ examples/extension/src/main/java/com/example/profile/PyroscopeProfile.java
@@ -15,6 +15,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.logging.Logger;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -22,23 +23,30 @@ import org.apache.http.impl.client.HttpClients;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-/** Pyrosope profiling. */
+/** Pyroscope profiling. */
 public class PyroscopeProfile {
+
+  private static Logger logger = Logger.getLogger("pyroscope-profile");
 
   /** Static method to start profiling. */
   public static void startProfiling() {
-    String tenantId = authenticateAndGetTenantId();
-    if (tenantId != null) {
-      PyroscopeAgent.start(
-          new Config.Builder()
-              .setApplicationName(SystemProperties.SERVICE_NAME)
-              .setProfilingEvent(EventType.ITIMER)
-              .setAllocLive(true)
-              .setServerAddress(EnvironmentConfig.MW_PROFILING_SERVER_URL)
-              .setTenantID(tenantId)
-              .build());
-    } else {
-      throw new RuntimeException("Authentication failed, tenant id not found");
+    try {
+      String tenantId = authenticateAndGetTenantId();
+      if (tenantId != null) {
+        PyroscopeAgent.start(
+            new Config.Builder()
+                .setApplicationName(SystemProperties.SERVICE_NAME)
+                .setProfilingEvent(EventType.ITIMER)
+                .setAllocLive(true)
+                .setServerAddress(EnvironmentConfig.MW_PROFILING_SERVER_URL)
+                .setTenantID(tenantId)
+                .build());
+      } else {
+        logger.warning("Profiling is not initiated as authentication is failed");
+      }
+    } catch (Exception e) {
+      logger.warning("Something went wrong while initialization of profiling");
+      e.printStackTrace();
     }
   }
 
@@ -52,7 +60,9 @@ public class PyroscopeProfile {
       // Data validation
       String mwApiKey = EnvironmentConfig.MW_API_KEY;
       if (mwApiKey == null) {
-        throw new RuntimeException("Environment variable MW_API_KEY is not provided");
+        logger.warning(
+            "Profiling is not initiated as environment variable MW_API_KEY is not provided");
+        return null;
       }
 
       // Build Request
@@ -86,7 +96,9 @@ public class PyroscopeProfile {
 
       return (String) jsonData.getOrDefault("account", null);
     } catch (Exception e) {
-      throw new RuntimeException(e.getMessage());
+      logger.severe(e.getMessage());
+      e.printStackTrace();
     }
+    return null;
   }
 }
