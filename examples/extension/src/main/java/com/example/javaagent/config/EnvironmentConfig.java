8ca162950b27d288fc3c2487611e7e4e91385f85
parth-dadhaniya <parth@middleware.io>
2023-12-19 18:23:40 +0530

    [ENG-985]: Add support of Memory allocation in profiling.

--- examples/extension/src/main/java/com/example/javaagent/config/EnvironmentConfig.java
+++ examples/extension/src/main/java/com/example/javaagent/config/EnvironmentConfig.java
@@ -11,6 +11,12 @@ public class EnvironmentConfig {
   public static final String MW_PROFILING_SERVER_URL =
       System.getenv().getOrDefault("MW_PROFILING_SERVER_URL", "https://profiling.middleware.io");
 
+  public static final String MW_PROFILING_ALLOC =
+      System.getenv().getOrDefault("MW_PROFILING_ALLOC", "512k");
+
+  public static final String MW_PROFILING_LOCK =
+      System.getenv().getOrDefault("MW_PROFILING_LOCK", "10ms");
+
   public static final String MW_AGENT_SERVICE =
       System.getenv().getOrDefault("MW_AGENT_SERVICE", "localhost");
 
